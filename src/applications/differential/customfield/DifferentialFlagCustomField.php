729b352bd5ecb90cf8dfd19ca0df259eb015ad41
Su Yang <soulteary@users.noreply.github.com>
2026-09-11 17:58:48 +0800

    cleanup: physically remove Differential application

--- src/applications/differential/customfield/DifferentialFlagCustomField.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-final class DifferentialFlagCustomField extends DifferentialCustomField {
-  use PhorgeFlagFlaggedObjectCustomField;
-}
--- src/applications/differential/xaction/DifferentialDiffTransactionType.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-abstract class DifferentialDiffTransactionType
-  extends PhabricatorModularTransactionType {}
